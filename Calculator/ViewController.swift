a88031e5cbc78656974e01d6bbc4e4bf1debd59f
Tristan Dolik <tristan.dolik@gmail.com>
2017-01-17 12:09:01 -0600

    Initial commit for push to public repo
    Signed-off-by: Tristan Dolik <tristan.dolik@gmail.com>

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,14 +12,79 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        displayValue = 0
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    var displayIsClean: Bool = true
+    var old: Double = 0
+    var op: String = "+"
+    
+    var displayValue: Double {
+        get {
+            return Double(Display.text!)!
+        }
+        set {
+            Display.text = "\(newValue)"
+        }
+    }
+    
+    @IBOutlet weak var Display: UILabel!
 
-
+    @IBAction func DigitPressed(_ sender: UIButton!) {
+        if(displayIsClean){
+            old = displayValue
+            Display.text = sender.currentTitle
+            displayIsClean = false;
+        } else {
+            appendDigit(sender.currentTitle!)
+        }
+        
+    }
+    
+    @IBAction func opPressed(_ sender: UIButton) {
+        let v = Evaluate()
+        if(!displayIsClean){
+            old = displayValue
+        }
+        
+        displayValue = v
+        op = sender.currentTitle!
+        displayIsClean = true
+    }
+    
+    @IBAction func clear() {
+        displayValue = 0
+        old = 0
+        op = "+"
+        displayIsClean = true
+    }
+    
+    func appendDigit(_ num :String){
+        Display.text!.append(num)
+    }
+    
+    
+    func Evaluate() -> Double {
+        
+        switch(op){
+        
+        case "=":
+            return displayValue
+        case "+":
+            return old + displayValue
+        case "−":
+            return old - displayValue
+        case "×":
+            return old * displayValue
+        case "÷":
+                return old / displayValue
+        default:
+            return 0.0
+        }
+    
+    }
 }
-
